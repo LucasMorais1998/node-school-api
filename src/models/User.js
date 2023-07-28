65115eb539563b8dbe5d941bbca0df4768a1478b
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-28 14:40:14 -0300

    create User model

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import bcrypt from 'bcryptjs';
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'O nome precisa ter entre 3 e 255 caracteres.',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            isEmail: {
+              msg: 'E-mail inválido.',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [8, 50],
+              msg: 'A senha precisa ter entre 8 e 50 caracteres.',
+            },
+          },
+        },
+      },
+      {
+        sequelize,
+      },
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcrypt.hash(user.password, 8);
+    });
+
+    return this;
+  }
+}
