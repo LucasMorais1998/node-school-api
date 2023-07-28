3c558ba5acc7d4b6ed57a660402bb35c867a846d
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-28 16:54:19 -0300

    config show in userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,6 +5,7 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.get('/', userController.index);
+router.get('/:id', userController.show);
 router.post('/', userController.store);
 
 export default router;
