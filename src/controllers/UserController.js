96622fe96279ebe0bf20f239a76464368511a4fd
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-28 20:34:04 -0300

    add update in UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -43,6 +43,36 @@ class UserController {
       });
     }
   }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({ errors: ['Usuário não encontrado.'] });
+      }
+
+      if (req.body.name === user.name && user.email === req.body.email) {
+        return res
+          .status(400)
+          .json({ errors: ['Os dados informados para atualização são iguais aos dados atuais.'] });
+      }
+
+      const updatedUser = await user.update(req.body);
+      return res.json(updatedUser);
+    } catch (error) {
+      console.error(error.errors.map((err) => err.message));
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 const userControllerInstance = new UserController();
