5453c810463644c91ef77701267a9322bb3945b4
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-28 21:50:26 -0300

    config destroy in userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,5 +8,6 @@ router.get('/', userController.index);
 router.get('/:id', userController.show);
 router.post('/', userController.store);
 router.put('/:id', userController.update);
+router.delete('/:id', userController.destroy);
 
 export default router;
