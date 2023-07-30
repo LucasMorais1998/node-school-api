83a6611767e7f254c2c4184ed265c0f1e24063b4
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-30 16:57:32 -0300

    config update in studentRoutes

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -5,6 +5,7 @@ import studentController from '../controllers/StudentController';
 const router = new Router();
 
 router.get('/:id', studentController.show);
+router.put('/:id', studentController.update);
 router.delete('/:id', studentController.destroy);
 
 export default router;
