12ca4f2998de401bf68fc333682d8891ca06c080
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-30 21:24:54 -0300

    add store in CourseController

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -11,6 +11,30 @@ class CourseController {
       });
     }
   }
+
+  async store(req, res) {
+    try {
+      const newCourse = await Course.create(req.body);
+
+      const {
+        id,
+        title,
+        description,
+        duration,
+      } = newCourse;
+
+      return res.status(201).json({
+        id, title, description, duration,
+      });
+    } catch (error) {
+      console.error(
+        error,
+      );
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 const courseControllerInstance = new CourseController();
