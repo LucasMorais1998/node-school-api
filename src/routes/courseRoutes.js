d4f326e7ed1985d6c2ce6dc5a2d5038870edceb7
LucasMorais1998 <lucas14.morais@gmail.com>
2023-07-31 21:58:44 -0300

    config update in courseRoutes

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -7,6 +7,7 @@ const router = new Router();
 router.get('/', courseController.index);
 router.get('/:id', courseController.show);
 router.post('/', courseController.store);
+router.put('/:id', courseController.update);
 router.delete('/:id', courseController.destroy);
 
 export default router;
