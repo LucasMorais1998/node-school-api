9d15beac2fab66954c9ef05afb8baa91d92a8043
LucasMorais1998 <lucas14.morais@gmail.com>
2023-08-14 21:35:39 -0300

    add store in PhotoController

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,7 +1,21 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('photo');
+
 class PhotoController {
   async store(req, res) {
     try {
-      return res.json('ok');
+      return upload(req, res, (err) => {
+        if (err) {
+          return res.status(400).json({
+            errors: [err.code],
+          });
+        }
+
+        return res.json(req.file);
+      });
     } catch (error) {
       console.error(error.errors.map((err) => err.message));
       return res.status(400).json({
