d542c44e4a1023f512bf5f21c29c0be83fb5313c
LucasMorais1998 <lucas14.morais@gmail.com>
2023-08-17 22:06:19 -0300

    config Photo in database index

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,13 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import Course from '../models/Course';
+import Photo from '../models/Photo';
 import Student from '../models/Student';
 import User from '../models/User';
 
-const models = [Student, User, Course];
+const models = [Student, User, Course, Photo];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
