8721a732b16e7077a4b0570f54396c321a3511f7
LucasMorais1998 <lucas14.morais@gmail.com>
2023-08-17 22:00:16 -0300

    create photos migration

---
+++ src/database/migrations/20230818000316-photos.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('photos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'students',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('photos');
+  },
+};
