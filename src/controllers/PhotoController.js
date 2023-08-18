3f26ab967233132cb003ed03f67332d0bbc4f4c1
LucasMorais1998 <lucas14.morais@gmail.com>
2023-08-17 22:18:40 -0300

    refactor store in the PhotoController

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,20 +1,26 @@
+/* eslint-disable camelcase */
 import multer from 'multer';
-
 import multerConfig from '../config/multer';
+
+import Photo from '../models/Photo';
 
 const upload = multer(multerConfig).single('photo');
 
 class PhotoController {
-  async store(req, res) {
+  store(req, res) {
     try {
-      return upload(req, res, (err) => {
+      return upload(req, res, async (err) => {
         if (err) {
           return res.status(400).json({
             errors: [err.code],
           });
         }
 
-        return res.json(req.file);
+        const { originalname, filename } = req.file;
+        const { student_id } = req.body;
+        const photo = await Photo.create({ originalname, filename, student_id });
+
+        return res.json(photo);
       });
     } catch (error) {
       console.error(error.errors.map((err) => err.message));
