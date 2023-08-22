c5efa0a39cd86d74999a607d997975f33f8963a2
LucasMorais1998 <lucas14.morais@gmail.com>
2023-08-21 23:46:23 -0300

    config destroy in courseStudentsRoutes

--- src/routes/courseStudentsRoutes.js
+++ src/routes/courseStudentsRoutes.js
@@ -4,5 +4,6 @@ import courseStudentsController from '../controllers/CourseStudentsController';
 const router = new Router();
 
 router.post('/', courseStudentsController.store);
+router.delete('/:student_id/:course_id', courseStudentsController.destroy);
 
 export default router;
