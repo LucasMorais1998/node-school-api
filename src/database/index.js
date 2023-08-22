e3a172b354426c23129014f3a14d708c0b7623ce
LucasMorais1998 <lucas14.morais@gmail.com>
2023-08-21 23:42:16 -0300

    config CourseStudents in database index

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,12 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import Course from '../models/Course';
+import CourseStudents from '../models/CourseStudents';
 import Photo from '../models/Photo';
 import Student from '../models/Student';
 import User from '../models/User';
 
-const models = [Student, User, Course, Photo];
+const models = [Student, User, Course, Photo, CourseStudents];
 
 const connection = new Sequelize(databaseConfig);
 
