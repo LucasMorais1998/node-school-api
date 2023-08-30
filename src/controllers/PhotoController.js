82e58276d9df88ba4a57288987389188255a44a1
LucasMorais1998 <lucas14.morais@gmail.com>
2023-08-29 23:33:28 -0300

    refactor PhotoController

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,22 +1,42 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import errorHandler from '../middlewares/errorHandlerMiddleware';
 
 import Photo from '../models/Photo';
 
 const upload = multer(multerConfig).single('photo');
 
 class PhotoController {
-  store(req, res) {
-    return upload(req, res, async (err) => {
-      if (err) {
-        return res.status(400).json({
-          errors: [err.code],
-        });
-      }
+  // eslint-disable-next-line consistent-return
+  async store(req, res) {
+    try {
+      await upload(req, res, async (err) => {
+        if (err) {
+          if (err.code === 'LIMIT_UNEXPECTED_FILE') {
+            return res.status(400).json({
+              errors: ['Invalid field name for file upload. Use "photo" field.'],
+            });
+          }
 
-      try {
+          return res.status(400).json({
+            errors: [err.code],
+          });
+        }
+
+        if (!req.file) {
+          return res.status(400).json({
+            errors: ['No file uploaded.'],
+          });
+        }
+
         const { originalname, filename } = req.file;
         const { student_id } = req.body;
+
+        if (!student_id) {
+          return res.status(400).json({
+            errors: ['student_id is required.'],
+          });
+        }
 
         const photo = await Photo.findOne({ where: { student_id } });
 
@@ -54,12 +74,10 @@ class PhotoController {
           filename: newFilename,
           student_id,
         });
-      } catch (error) {
-        return res.status(400).json({
-          errors: error.errors.map((e) => e.message),
-        });
-      }
-    });
+      });
+    } catch (error) {
+      return errorHandler(error, req, res);
+    }
   }
 }
 
